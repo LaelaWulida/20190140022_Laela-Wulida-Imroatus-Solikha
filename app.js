4bfc7089240b3402c1d1cc5b033537ee0df267cc
LaelaWulida <laela27811@gmail.com>
2021-03-28 16:22:05 +0700

    laela

---
+++ app.js
@@ -0,0 +1,28 @@
+//Alert dari app.js
+alert('Ini alert dari app.js')
+
+//Confirm dari app.js
+confirm('Ini confirm dari app.js')
+
+//Console.log
+console.log('ini console.log dari app.js')
+
+Date()
+
+//Variable javascript
+//var, let, const
+let a = 1
+let b = 3
+let c = a + b
+//Di phyton console.log itu sama dengan print
+console.log(c)
+
+//Tanggal
+Date()
+let tanggal = Date()
+alert(tanggal)
+
+
+//DOM Selector HTML
+//Document Object Model
+document.getElementById('nama').innerHTML="LaelaWulida"
